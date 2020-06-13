d78645f0f58baac8e74a63b50ac5d0ee2c21ad29
omelendrez <omar.melendrez@gmail.com>
2020-06-13 01:21:35 -0300

    Resolve #9 Resolve #10 Resolve #11

---
+++ models/db.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql')
+
+const pool = mysql.createPool({
+  connetionLimit: 10,
+  host: process.env.SQL_SERVER,
+  database: process.env.SQL_DATABASE,
+  user: process.env.SQL_USER,
+  password: process.env.SQL_PASSWORD,
+})
+
+module.exports = pool
