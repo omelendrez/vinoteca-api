3014d510acf76475fe014a0d65d05e49b2529508
omelendrez <omar.melendrez@gmail.com>
2020-06-13 18:05:43 -0300

    Resolves #28

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const user = require('./user')
 const company = require('./company')
+const stock = require('./stock')
 
 module.exports = (router) => {
   user(router)
   company(router) // Cada vez que agregamos una nueva ruta con su respectivo modelo y controlador, debemos importarla arriba y agregarla acá
+  stock(router)
   return router
 }
