c9563959ea395f9458d1ff198a9bb8b7ec18ab77
Ema <emarengo73@gmail.com>
2020-06-16 20:23:54 -0300

    resolve #84

--- test/store.js
+++ test/store.js
@@ -10,11 +10,11 @@ chai.should()
 chai.use(chaiHttp)
 
 
-describe('CATEGORIAS', () => {
+describe('Depósitos', () => {
 
-  it('Recrear tabla category', (done) => {
+  it('Recrear tabla store', (done) => {
     chai.request(server)
-      .post('/api/v1/reset-db/category')
+      .post('/api/v1/reset-db/store')
       .end((err, res) => {
 
         res.should.have.status(200)
@@ -30,9 +30,9 @@ describe('CATEGORIAS', () => {
       })
   }).timeout(5000)
 
-  it('Debería devolver todas las categorías', (done) => {
+  it('Debería devolver todos los depósitos', (done) => {
     chai.request(server)
-      .get('/api/v1/categories')
+      .get('/api/v1/stores')
       .end((err, res) => {
 
         res.should.have.status(200)
@@ -50,15 +50,18 @@ describe('CATEGORIAS', () => {
       })
   }).timeout(5000)
 
-  it('Debería crear una nueva categoría', (done) => {
-    const category = {
-      "name": "Vinos",
-      "code": "123456",
+  it('Debería crear un nuevo depósito', (done) => {
+    const store = {
+      "name": "Almacen Pepito",
+      "contact": "El perro de Pepito 11221",
+      "address": "Juan Molina y Avellaneda",
+      "phone": "2912222222",
+      "email": "ap@gmail.com",
       "companyId": "1"
     }
     chai.request(server)
-      .post('/api/v1/categories')
-      .send(category)
+      .post('/api/v1/stores')
+      .send(store)
       .end((err, res) => {
 
         res.should.have.status(201)
@@ -75,21 +78,21 @@ describe('CATEGORIAS', () => {
       })
   }).timeout(5000)
 
-  it('Debería devolver una categoría', (done) => {
+  it('Debería devolver un depósito', (done) => {
     chai.request(server)
-      .get('/api/v1/categories/1')
+      .get('/api/v1/stores/1')
       .end((err, res) => {
 
         res.should.have.status(200)
 
         res.body.should.be.a('object')
 
-        res.body.should.have.property('code')
+        res.body.should.have.property('contact')
         res.body.should.have.property('name')
         res.body.should.have.property('companyId')
 
-        res.body.code.should.be.eql('123456')
-        res.body.name.should.be.eql('Vinos')
+        res.body.contact.should.be.eql('El perro de Pepito 11221')
+        res.body.name.should.be.eql('Almacen Pepito')
 
         res.body.companyId.should.be.eql(1)
 
@@ -98,9 +101,9 @@ describe('CATEGORIAS', () => {
       })
   }).timeout(5000)
 
-  it('Debería devolver todas las categorías (el registro insertado)', (done) => {
+  it('Debería devolver todos los depósitos (el registro insertado)', (done) => {
     chai.request(server)
-      .get('/api/v1/categories')
+      .get('/api/v1/stores')
       .end((err, res) => {
 
         res.should.have.status(200)
@@ -116,15 +119,18 @@ describe('CATEGORIAS', () => {
       })
   }).timeout(5000)
 
-  it('Debería modificar una categoría', (done) => {
-    const category = {
-      name: "Vinoteca & Fiambreria Gourment",
-      code: "123456",
-      companyId: 2
+  it('Debería modificar un depósito', (done) => {
+    const store = {
+      "name": "El garage de la esquina",
+      "contact": "El gato de Pepito 11221",
+      "address": "Juan Molina 12",
+      "phone": "2912211222",
+      "email": "aaa@gmail.com",
+      "companyId": "2"
     }
     chai.request(server)
-      .put('/api/v1/categories/1')
-      .send(category)
+      .put('/api/v1/stores/1')
+      .send(store)
       .end((err, res) => {
 
         res.should.have.status(200)
@@ -134,12 +140,12 @@ describe('CATEGORIAS', () => {
         res.body.should.have.property('errors')
         res.body.should.have.property('data')
 
-        res.body.data.should.have.property('code')
+        res.body.data.should.have.property('phone')
         res.body.data.should.have.property('name')
         res.body.data.should.have.property('companyId')
 
-        res.body.data.code.should.be.eql('123456')
-        res.body.data.name.should.be.eql('Vinoteca & Fiambreria Gourment')
+        res.body.data.phone.should.be.eql('2912211222')
+        res.body.data.name.should.be.eql('El garage de la esquina')
 
         res.body.data.companyId.should.be.eql(2)
 
@@ -148,9 +154,9 @@ describe('CATEGORIAS', () => {
       })
   }).timeout(5000)
 
-  it('Debería eliminar una categoría', (done) => {
+  it('Debería eliminar un depósito', (done) => {
     chai.request(server)
-      .delete('/api/v1/categories/1')
+      .delete('/api/v1/stores/1')
       .end((err, res) => {
 
         res.should.have.status(200)
