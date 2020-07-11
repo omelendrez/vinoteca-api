f2cdcd24a45d50640e9a55f42fc661bf7ae5b3ac
omelendrez <omar.melendrez@gmail.com>
2020-07-11 00:36:43 -0300

    Agregar cantidad de items al objeto order

--- models/queries/all/order.sql
+++ models/queries/all/order.sql
@@ -1,4 +1,5 @@
-SELECT o.id,
+SELECT
+  o.id,
   o.number,
   o.date,
   o.amount,
@@ -15,10 +16,20 @@ SELECT o.id,
     WHEN o.status_id = 2 THEN 'Enviada'
     WHEN o.status_id = 3 THEN 'Recibida'
     WHEN o.status_id = 4 THEN 'Cancelada'
-  END AS 'status_name'
-FROM `order` as o
+  END AS 'status_name',
+  (
+    SELECT
+      COUNT(*)
+    FROM
+      `order_details`
+    WHERE
+      order_id = o.id
+  ) AS 'items'
+FROM
+  `order` as o
   INNER JOIN `company` as co ON o.company_id = co.id
   INNER JOIN `supplier` as su ON o.supplier_id = su.id
   INNER JOIN `user` as u1 ON o.created_by = u1.id
   LEFT OUTER JOIN `user` as u2 ON o.updated_by = u2.id
-ORDER BY o.id DESC;
+ORDER BY
+  o.id DESC;
