295247368031a2ead760fe24d590adc8fd03af96
omelendrez <omar.melendrez@gmail.com>
2020-09-20 15:44:06 -0300

    Fix default value on graph_data schema

--- models/schema/graph_data.sql
+++ models/schema/graph_data.sql
@@ -4,8 +4,8 @@ CREATE TABLE graph_data (
   id INTEGER NOT NULL AUTO_INCREMENT,
   period CHAR(6) NOT NULL,
   period_name CHAR(7) NOT NULL,
-  purchases DECIMAL(12, 2) DEFAULT(0),
-  sales DECIMAL(12, 2) DEFAULT(0),
+  purchases DECIMAL(12, 2) DEFAULT 0,
+  sales DECIMAL(12, 2) DEFAULT 0,
   company_id INTEGER NOT NULL,
   PRIMARY KEY (id),
   INDEX(company_id, period)
